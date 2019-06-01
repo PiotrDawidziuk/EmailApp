18fb9fa9493132ca4e99a7b70f552209391643c1
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-01 20:35:55 +0200

    edtexts added java

--- app/src/main/java/pl/piotrdawidziuk/emailapp/MainActivity.java
+++ app/src/main/java/pl/piotrdawidziuk/emailapp/MainActivity.java
@@ -2,8 +2,14 @@ package pl.piotrdawidziuk.emailapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextTo;
+    private EditText editTextSubject;
+    private EditText editTextMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
